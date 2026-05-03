c7970ecef8fd269e3aa73e2798bc8a98116cf731
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2026-05-03 18:13:25 +0530

    front

--- Class1/app.js
+++ Class1/app.js
@@ -1,2 +1,13 @@
-a = 5;
-console.log(a);
+
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(function log() {
+    // console.log(i); 
+  }, 1000);
+}
+
+let b = 100;
+// let b = 100;
+console.log(b);
+
+//   console.log(b);
