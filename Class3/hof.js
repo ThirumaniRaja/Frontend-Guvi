5122b550ba9e60950c856a27df8c760ca547c9c6
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2026-05-08 20:11:45 +0530

    hof

---
+++ Class3/hof.js
@@ -0,0 +1,11 @@
+
+ function sayHello(){
+console.log("Hi How are you?");
+}
+
+function greet(name, callback) {
+    console.log("Hello, " + name + "!");
+    callback();
+}
+
+greet("Thiru", sayHello);
