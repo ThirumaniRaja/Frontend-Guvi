e36a6825bdf53b4ef7845f6e3476471c2cc6cd88
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2026-05-09 19:08:33 +0530

    class4

---
+++ Class4/array.js
@@ -0,0 +1,6 @@
+const stack = new Array(1,2,3,4,5);
+stack.push(6);
+stack.unshift(0);
+stack.pop();
+stack.shift();
+console.log(stack);
---
+++ Class4/array1.js
@@ -0,0 +1,9 @@
+const myArray = [1, 2, 3, 4, 5,8,300,710,7];
+myArray.sort((a,b)=> a - b);
+console.log(myArray);
+
+
+const stack = ["","","g","h","i"];
+stack.fill("hello");
+// stack.map("hi");
+console.log(stack);
