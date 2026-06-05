54ca3123066fbf50b8f5955f2445e76f8fec907e
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2026-06-05 18:41:43 +0530

    adv-js

---
+++ Class13-Advance-JS/main.js
@@ -0,0 +1,18 @@
+// ...existing code...
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+const firstName = "venkat";
+const lastName = "ram";
+const age = 20;
+
+const mobile = "iphone";
+
+// export at bottom
+export { add, subtract, firstName, lastName, age, mobile }
+// ...existing code...
---
+++ Class13-Advance-JS/myfile.js
@@ -0,0 +1,8 @@
+import * as common from "./main.js";
+
+console.log(common.add(5, 3)); // Output: 8
+console.log(common.subtract(10, 4)); // Output: 6
+console.log(common.firstName); // Output: John
+console.log(common.lastName); // Output: Doe
+console.log(common.age); // Output: 30
+console.log(common.mobile); // Output: iphone   
