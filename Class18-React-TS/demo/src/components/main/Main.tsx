ce25b21696744578a9b060c60dac9b3d4d3c2b16
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2026-06-19 19:40:18 +0530

    props

---
+++ Class18-React-TS/demo/src/components/main/Main.tsx
@@ -0,0 +1,16 @@
+// ...existing code...
+import "./Main.css"
+
+function Main() {
+  return (
+    <main className="main">
+      <h2>Main content of the page</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+        expedita.
+      </p>
+    </main>
+  )
+}
+
+export default Main
