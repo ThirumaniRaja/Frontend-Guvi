97250598f70392d11dc60444e39f7a4e13d6e7df
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2026-06-19 19:01:38 +0530

    react-tsx

---
+++ Class18-React-TS/demo/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+
+
+function App() {
+  
+
+  return (
+   <div className="container">
+    <Header></Header>
+    <Navbar></Navbar>
+
+   </div>
+  )
+}
+
+export default App
